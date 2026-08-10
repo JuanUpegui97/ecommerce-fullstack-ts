027d6a5a9bb15bac59821cfca7bf6c29db46d3ba
Juan Upegui <juan.upeguil@comunidad.iush.edu.co>
2026-08-09 20:36:31 -0500

    middlewares

---
+++ ecommerce-backend/src/middlewares/auth.middleware.ts
@@ -0,0 +1,42 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+
+const JWT_SECRET = process.env.JWT_SECRET as string;
+
+export const authMiddleware = (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader) {
+        return res.status(401).json({
+            error: "Token requerido"
+        });
+    }
+
+    const token = authHeader.split(" ")[1];
+
+    try {
+
+        const decoded = jwt.verify(token, JWT_SECRET);
+
+        if (typeof decoded === "string") {
+            return res.status(401).json({
+                error: "Token inválido"
+            });
+        }
+
+        req.user = decoded;
+
+        next();
+
+    } catch (error) {
+
+        return res.status(401).json({
+            error: "Token inválido o expirado"
+        });
+    }
+};
--- ecommerce-backend/src/routes/index.ts
+++ ecommerce-backend/src/routes/index.ts
@@ -1,12 +1,17 @@
 import { Router } from "express";
 import userRoutes from "./user.routes.js";
 import loginRoutes from "./login.routes.js";
+import { authMiddleware } from "../middlewares/auth.middleware.js";
 
 
 
 const router = Router();
 
-router.use(userRoutes);
+
 router.use(loginRoutes);
+
+router.use(authMiddleware);
+
+router.use(userRoutes);
 
 export default router;
